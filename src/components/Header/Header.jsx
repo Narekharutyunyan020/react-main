3987588adfd2275a18d044644c3659f6f90941c7
Narekharutyunyan020 <harutyunyannarek896@gmail.com>
2025-05-11 14:00:33 +0400

    menu icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 
 import { Link } from "react-router-dom"
 import "./Header.css"
+import Menuicons from "../../icons/menuicons"
+import { useState } from "react"
 
 function Header() {
+  const [menuState, setMenuState] = useState(false)
+  
   return <header className="header">
     <div className="container">
       <div className="logoImg">
@@ -11,6 +15,7 @@ function Header() {
       </div>
 
       <nav>
+
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><a href="#portfolio">Portfolio</a></li>
@@ -18,6 +23,15 @@ function Header() {
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
+      <button className="btn" onClick={()=>setMenuState(!menuState)}>
+        <Menuicons size={40} color="white" />
+      </button>
+      <div style={{ left: menuState ? '0' : '-100%' }} className="menuBlock">
+        
+        
+      
+      </div>
+  
     </div>
   </header>
 
